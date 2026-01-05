0d31bf7f7f406f50f289a8ebc996bf09b2d6ea37
MadHatter4603 <dariopaz2003@gmail.com>
2026-01-05 16:43:11 +0100

    Update cache version to ricette-v1.2

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "ricette-v1.1";
+const CACHE = "ricette-v1.2";
 
 self.addEventListener("install", e => {
   e.waitUntil(
@@ -13,6 +13,7 @@ self.addEventListener("fetch", e => {
     caches.match(e.request).then(r => r || fetch(e.request))
   );
 });
+
 
 
 
