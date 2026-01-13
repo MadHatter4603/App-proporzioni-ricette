dda19880d6c717816032abccf21b37f3111c4dba
MadHatter4603 <dariopaz2003@gmail.com>
2026-01-13 20:38:38 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -14,6 +14,47 @@ self.addEventListener("fetch", e => {
   );
 });
 
+/*const CACHE = "ricette-v2.7";
+
+self.addEventListener("install", e => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE).then(c =>
+      c.addAll([".", "index.html", "style.css", "app.js"])
+    )
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(names =>
+      Promise.all(
+        names.filter(n => n !== CACHE).map(n => caches.delete(n))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", e => {
+  if (e.request.mode === "navigate") {
+    e.respondWith(
+      fetch(e.request)
+        .then(r => {
+          const clone = r.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+          return r;
+        })
+        .catch(() => caches.match(e.request))
+    );
+    return;
+  }
+
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});*/
+
 
 
 
